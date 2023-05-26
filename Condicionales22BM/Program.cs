b5bd1d1bd87fffd1f1a47e3cf0c0975c60a15fa0
RECSTEINS <raulcedeno_manzanarez@hotmail.com>
2023-05-26 07:59:08 -0500

    Agregar archivos de proyecto.

---
+++ Condicionales22BM/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Condicionales22BM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales if, else
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("--------Tramite Licencia---------");
+            Console.WriteLine("---------------------------------\n");
+            Console.WriteLine("Ingresa tu nombre: ");
+            string Nombre = Console.ReadLine();
+
+            Console.WriteLine("\nIngresa tu edad: ");
+            int Edad = int.Parse(Console.ReadLine());
+
+            if (Edad >= 18)
+            {
+                Console.WriteLine("Ingresa el pago de 500");
+                int pago = int.Parse(Console.ReadLine());
+                if (pago >= 500)
+                {
+                    Console.WriteLine("Pago aceptado.");
+                    Console.WriteLine("Tramite realizado.");
+                }
+                else
+                {
+                    Console.WriteLine("\nEl pago no esta completo.");
+                    pago = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Tramite realizado.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No es posible realizar en este momento tu tramite.");
+            }
+
+        }
+    }
+}
